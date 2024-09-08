126fd6f94894f13883901b8ef31a8f8a5055be43
konok101 <rockykonok@gmail.com>
2024-09-08 17:46:33 +0600

    ss

--- Models/MessageStatus.cs
+++ Models/MessageStatus.cs
@@ -5,7 +5,7 @@
         public object Data { get; set; } // Data related to the error (optional)
         public bool Status { get; set; } // Indicates the status of the operation
         public int Code { get; set; } // HTTP status code or custom error code
-        public string Message { get; set; } // Error message
+        public string Message { get; set; } // Error message s
     }
 
 }
